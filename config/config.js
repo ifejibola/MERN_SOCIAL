55e1a892f3393fc0fccffdcf2b08e97c060fe057
Paul Ajibola <ife.jibola@gmail.com>
2021-02-03 11:04:55 -0500

    mlab-atlass migration

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   cookieSecret: '',
   jwtSecret: '123Secret',
   PORT: process.env.PORT || 3000,
-  mongo: '' || process.env.MONGODB_URI || process.env.MONGO_HOST
+  mongo: 'mongodb+srv://admin:admin123@mernauth.gezyk.mongodb.net/mernauth?retryWrites=true&w=majority' || process.env.MONGODB_URI || process.env.MONGO_HOST
 };
 module.exports = config
 // export default config
